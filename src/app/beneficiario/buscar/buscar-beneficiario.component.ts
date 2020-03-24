bc96f4a06065085e610d42ba6ddc4d925ad6fb7d
areangonzalez <areangonzalez@gmail.com>
2020-03-24 04:19:33 -0300

    TG-98 #Closed se crea aplica funcionamiento para el formulario de busqueda avanzada

--- src/app/beneficiario/buscar/buscar-beneficiario.component.ts
+++ src/app/beneficiario/buscar/buscar-beneficiario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'buscar-beneficiario',
@@ -18,9 +19,19 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
     ]
 })
 export class BuscarBeneficiarioComponent implements OnInit {
-  public state: string = 'small';
+  public state: string = 'small'; // estado de la animacion
+  public realizoBusqueda: boolean = false; // aplica filtro a boton de busqueda avanzada
+  public global_param: string = ""; // parametro de busqueda global para beneficiario
+  public busquedaAvanzadaForm: FormGroup;
 
-  constructor(){}
+  constructor( private _fb: FormBuilder ){
+    this.busquedaAvanzadaForm = _fb.group({
+      localidadid: '',
+      calle: '',
+      barrio: '',
+      estado: ''
+    });
+  }
 
   ngOnInit(){}
 
@@ -30,5 +41,56 @@ export class BuscarBeneficiarioComponent implements OnInit {
   abrirCerrarBusquedaAvanzada() {
     this.state = (this.state === 'small' ? 'large' : 'small');
   }
-
+  /**
+   * Armo el api de busqueda para enviarlo al componente padre
+   */
+  buscar() {
+    let busquedaAvanzada = this.busquedaAvanzadaForm.value;
+    let apiBusqueda: any= {};
+    let esTrue: boolean = false;
+    console.log(busquedaAvanzada);
+    if (this.global_param !== '') {
+      Object.assign(apiBusqueda, {"global_param": this.global_param});
+    }
+    for (const clave in busquedaAvanzada) {
+      if(busquedaAvanzada[clave] !== '' && busquedaAvanzada[clave] !== null && (busquedaAvanzada[clave])){
+        Object.assign(apiBusqueda, {[clave]: busquedaAvanzada[clave]});
+        esTrue = true;
+      }
+    }
+    console.log(esTrue);
+    this.realizoBusqueda = esTrue;
+    this.state = (esTrue) ? 'large' : 'small';
+    //this.obtenerBusqueda.emit(apiBusqueda);
+  }
+  /**
+   * Limpiar los campos de busqueda
+   */
+  limpiarCampos() {
+    let busqueda: any = this.busquedaAvanzadaForm.value;
+    for (const key in busqueda) {
+      if (key == 'fechaDesde') {
+        busqueda[key] = null;
+      }else if (key == 'fechaHasta') {
+        busqueda[key] = null;
+      }else {
+        busqueda[key] = '';
+      }
+    }
+    this.global_param = '';
+    this.busquedaAvanzadaForm.patchValue(busqueda);
+    this.realizoBusqueda = false;
+    this.state = 'small';
+    //this.limpiar.emit(true);
+  }
+  /**
+     * Resalta los campos que han sido utilizados en búsqueda
+     * @param valor [any] contiene el valor de la variable
+     * @return [boolean] devuelve el valor a resaltar en booleano
+     */
+    resaltarCampo(valor:any) {
+      let resaltar: boolean = false;
+      resaltar = (valor != null && valor != '') ? true : false;
+      return resaltar;
+    }
 }
