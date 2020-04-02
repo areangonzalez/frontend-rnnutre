0de75d7912d794845a032056ce255598396def3a
areangonzalez <areangonzalez@gmail.com>
2020-04-02 11:56:43 -0300

    TG-121 #Closed se aplica el cierre de sesion

--- src/app/shared/layout/componentes/cabecera.component.ts
+++ src/app/shared/layout/componentes/cabecera.component.ts
@@ -28,7 +28,7 @@ export class CabeceraComponent implements OnInit {
       setTimeout(() => {
         this._authentication.logout();
         this._loaderService.hide();
-        this._router.navigate(['/login']);
+        this._router.navigate(['/']);
       }, 1000);
     }
 
