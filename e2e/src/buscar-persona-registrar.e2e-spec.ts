ac009d8881f8f2428c2d2bedab6084d4d80d4310
areangonzalez <areangonzalez@gmail.com>
2020-04-07 12:31:40 -0300

    TG-130 #closed se busca una persona registrada en el sistema TG-131 #Closed se verifica el ingreso de caracteres TG-132 #Closed se genera un archivo de buscarPersonaPage TG-127 #Done se testea el componente de buscar persona

--- e2e/src/buscar-persona-registrar.e2e-spec.ts
+++ e2e/src/buscar-persona-registrar.e2e-spec.ts
@@ -5,7 +5,7 @@ import { BuscarPersonaPage } from './page-object/buscar-persona.po';
 describe('Iniciando test', () => {
   let buscarPersona: BuscarPersonaPage;
 
-  beforeAll(() => {
+  beforeEach(() => {
     browser.get('/');
 
     buscarPersona = new BuscarPersonaPage();
@@ -20,6 +20,28 @@ describe('Iniciando test', () => {
     browser.waitForAngular();
 
     expect(buscarPersona.regitrar().isPresent()).toBeTruthy();
+  });
+
+  it('Busco una persona que este registrada dentro del sistema', () => {
+    // ingreo numero de documento
+    buscarPersona.ingresarDocumento('29232132');
+    // hago click en buscar
+    buscarPersona.buscar();
+    // espero la respuesta
+    browser.waitForAngular();
+    // Obtengo mensaje
+    let mensaje = buscarPersona.buscarPersonaElemento().element(by.css('div.d-flex')).element(by.css('div.alert')).getText();
+    // confirmo el error
+    expect(mensaje).toBe('Esta persona ya ha sido registrada.');
+  });
+
+  it('Verifico los campos ingresando caracteres', () => {
+    // ingreso una palabra
+    buscarPersona.ingresarDocumento('hola');
+    // obtengo el valor del elemento
+    let valueElement = buscarPersona.buscarPersonaElemento().element(by.id('buscar-persona')).getAttribute('value');
+    // verifico que este en vacio
+    expect(valueElement).toEqual('');
   });
 
 });
